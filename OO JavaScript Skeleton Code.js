9aaf45c9b3a3706b54cc1870341b8de7cc51b02f
rdli <rdli.data@gmail.com>
2014-12-08 17:31:27 -0500

    Update OO JavaScript Skeleton Code.js

--- OO JavaScript Skeleton Code.js
+++ OO JavaScript Skeleton Code.js
@@ -65,12 +65,12 @@ var baseClass = function ()
 	
 	this.validUrl = function (str) {
 			var pattern = new RegExp(
-								  "^(ftp:\/\/|http:\/\/|https:\/\/|\/\/)" +
-								  "((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))" +
-								  "(\:\d+)?(\/[-a-z\d%_.~+=]*)*" +
-								  "(\?[;&a-z\d%_.~+=-]*)?" +
-								  "(\#[-a-z\d_]*)?$", 
-								"i");
+						 "^(ftp:\/\/|http:\/\/|https:\/\/|\/\/)" +
+						 "((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))" +
+						 "(\:\d+)?(\/[-a-z\d%_.~+=]*)*" +
+						 "(\?[;&a-z\d%_.~+=-]*)?" +
+						 "(\#[-a-z\d_]*)?$", 
+					"i");
 			if (pattern.test(str)) {
 				return true;
 			} else {
